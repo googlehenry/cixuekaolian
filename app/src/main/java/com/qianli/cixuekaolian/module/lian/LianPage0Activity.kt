9706d5fb6622f7cad4ff0bdb0d8aeeb532f278b4
a5365958 <2571706435@qq.com>
2021-03-20 13:12:08 +0800

    effects

--- app/src/main/java/com/qianli/cixuekaolian/module/lian/LianPage0Activity.kt
+++ app/src/main/java/com/qianli/cixuekaolian/module/lian/LianPage0Activity.kt
@@ -2,6 +2,7 @@ package com.qianli.cixuekaolian.module.lian
 
 import com.qianli.cixuekaolian.R
 import com.qianli.cixuekaolian.base.BaseActivity
+import kotlinx.android.synthetic.main.activity_lian_item_page.*
 
 class LianPage0Activity : BaseActivity() {
 
@@ -11,6 +12,82 @@ class LianPage0Activity : BaseActivity() {
 
     override fun afterCreated() {
         supportActionBar?.hide()
+
+        item_main.text = """
+            C
+          
+            Race walking shares many fitness benefits with running, research shows, while most likely contributing to fewer injuries. It does, however, have its own problem.
+
+            Race walkers are conditioned athletes. The longest track and field event at the Summer Olympics is the 50-kilometer race walk, which is about five miles longer than the marathon. But the sport’s rules require that a race walker’s knees stay straight through most of the leg swing and one foot remain in contact(接触) with the ground at all times. It’s this strange form that makes race walking such an attractive activity, however, says Jaclyn Norberg, an assistant professor of exercise science at Salem State University in Salem, Mass.
+
+            Like running, race walking is physically demanding, she says. According to most calculations, race walkers moving at a pace of six miles per hour would burn about 800 calories(卡路里) per hour, which is approximately twice as many as they would burn walking, although fewer than running, which would probably burn about 1,000 or more calories per hour.
+
+            However, race walking does not pound the body as much as running does, Dr. Norberg says. According to her research, runners hit the ground with as much as four times their body weight per step, while race walkers, who do not leave the ground, create only about 1.4 times their body weight with each step.
+
+            As a result, she says, some of the injuries associated with running, such as runner’s knee, are uncommon among race walkers. But the sport’s strange form does place considerable stress on the ankles and hips, so people with a history of such injuries might want to be cautious in adopting the sport. In fact, anyone wishing to try race walking should probably first consult a coach or experienced racer to learn proper technique, she says. It takes some practice.
+      
+      
+            28. Why are race walkers conditioned athletes?
+
+            A. They must run long distances.
+
+            B. They are qualified for the marathon.
+
+            C. They have to follow special rules.
+
+            D. They are good at swinging their legs.
+
+            29. What advantage does race walking have over running?
+
+            A. It’s more popular at the Olympics.
+
+            B. It’s less challenging physically.
+
+            C. It’s more effective in body building.
+
+            D. It’s less likely to cause knee injuries.
+
+            30 What is Dr. Norberg’s suggestion for someone trying race walking?
+
+            A. Getting experts’ opinions.B. Having a medical checkup.
+
+            C. Hiring an experienced coach.D. Doing regular exercises.
+
+            31. Which word best describes the author’s attitude to race walking?
+
+            A. Skeptical.B. Objective.C. Tolerant.D. Conservative.
+        """
     }
+    /*
+    28. Why are race walkers conditioned athletes?
+
+            A. They must run long distances.
+
+            B. They are qualified for the marathon.
+
+            C. They have to follow special rules.
+
+            D. They are good at swinging their legs.
+
+            29. What advantage does race walking have over running?
+
+            A. It’s more popular at the Olympics.
+
+            B. It’s less challenging physically.
+
+            C. It’s more effective in body building.
+
+            D. It’s less likely to cause knee injuries.
+
+            30 What is Dr. Norberg’s suggestion for someone trying race walking?
+
+            A. Getting experts’ opinions.B. Having a medical checkup.
+
+            C. Hiring an experienced coach.D. Doing regular exercises.
+
+            31. Which word best describes the author’s attitude to race walking?
+
+            A. Skeptical.B. Objective.C. Tolerant.D. Conservative.
+     */
 
 }
